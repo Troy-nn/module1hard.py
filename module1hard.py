9f22b86961d2012b2286973ac25d37d894f8fcf8
Troy-nn <nrgnnrf@gmail.com>
2024-07-14 08:23:42 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,10 +1,19 @@
-grades = ([5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5])
+grades = [5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]
 students = ('Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron')
-result =  zip(grades, students)
-resres = (list(result))
+students = sorted(students)
+aaron = sum(grades[0]) / len(grades[0])
+bilbo = sum(grades[1]) / len(grades[1])
+johny = sum(grades[2]) / len(grades[2])
+ked = sum(grades[3]) / len(grades[3])
+Stv = sum(grades[4]) / len(grades[4])
 
 
-students = (sorted(list(students)))
-itog = ((students[0],  sum(grades[0]) / len(grades[0])), (students[1],  sum(grades[1]) / len(grades[1])), (students[2],  sum(grades[2]) / len(grades[2])), (students[3],  sum(grades[3]) / len(grades[3])), (students[4],  sum(grades[4]) / len(grades[4])))
-print(dict(itog))
+grades = aaron,bilbo, johny, ked, Stv
+
+dict(zip(students,grades))
+print(dict(zip(students,grades)))
+
+# print(dict(zip(students,grades)))
+# itog = ((students[0],  sum(grades[0]) / len(grades[0])), (students[1],  sum(grades[1]) / len(grades[1])), (students[2],  sum(grades[2]) / len(grades[2])), (students[3],  sum(grades[3]) / len(grades[3])), (students[4],  sum(grades[4]) / len(grades[4])))
+# print(dict(itog))
 
